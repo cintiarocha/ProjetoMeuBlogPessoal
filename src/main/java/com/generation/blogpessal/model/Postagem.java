b4c80ad6d589f9c401aded9a7f18521e6f33f112
Cintia Rocha <cintia.rocha95@gmail.com>
2022-05-06 14:16:14 -0300

    Metódos do CRUD

--- src/main/java/com/generation/blogpessal/model/Postagem.java
+++ src/main/java/com/generation/blogpessal/model/Postagem.java
@@ -1,33 +1,45 @@
 package com.generation.blogpessal.model;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-@Entity
-@Table (name= "tb_postagem")
-public class Postagem {
+import org.springframework.lang.NonNull;
 
+@Entity
+@Table(name = "postagem")
+
+public class Postagem {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	public Long id;
+	private long id;
+	
+	@NonNull
+	@Size(min = 5, max = 100)
+	private String titulo;
 	
 	@NotNull
-	@Size(min = 5, max = 100, message= "O campo deve terno mínimo 5 caracteres, e no maximo 100 caracteres")
-	public String titulo;
+	@Size(min = 10, max = 500)
+	private String texto;
 	
-	@NotNull
-	public String texto;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date date = new java.sql.Date(System.currentTimeMillis());
 
-	public Long getId() {
+
+	public long getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
+	public void setId(long id) {
 		this.id = id;
 	}
 
@@ -46,9 +58,13 @@ public class Postagem {
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
-	
 
-	
-	
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	} 
 
 }
