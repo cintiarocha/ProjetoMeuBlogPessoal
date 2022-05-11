c0f2ce7c4f2958405c8817871e7eb125a9cc25cf
Cintia Rocha <cintia.rocha95@gmail.com>
2022-05-11 08:30:58 -0300

    descrição breve do commit

--- src/main/java/com/generation/blogpessal/Controller/PostagemController.java
+++ src/main/java/com/generation/blogpessal/Controller/PostagemController.java
@@ -88,4 +88,5 @@ public class PostagemController {
 				.orElse(ResponseEntity.notFound().build());
 	}
 	
+	
 }
--- src/main/java/com/generation/blogpessal/Repository/TemaRepository.java
+++ src/main/java/com/generation/blogpessal/Repository/TemaRepository.java
@@ -3,10 +3,11 @@ package com.generation.blogpessal.Repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 
 import com.generation.blogpessal.model.TemaModel;
-
+@Repository
 public interface TemaRepository extends JpaRepository<TemaModel, Long> {
 	public List<Model> findAllByDescricaoContainingIgnoreCase(String descricao);
 	
